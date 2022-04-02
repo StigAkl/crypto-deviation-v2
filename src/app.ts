3d4984145b9d9cc55fd3e1238fa4c5c460d00e44
stigakl <stigakl@outlook.com>
2022-04-02 15:49:16 +0200

    remove suppression for some time

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const performAnalysis = async (timeFrame: Timeframe, stdDev: number = 3) => {
     const currency = markets[i];
 
     if (!shouldPerformAnalysis(currency, timeFrame)) {
-      continue;
+      //continue;
     }
 
     const {
