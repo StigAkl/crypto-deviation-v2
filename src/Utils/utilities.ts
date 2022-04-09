b0506f1bb94213b18b580f33f2234951a20b9071
stigakl <stigakl@outlook.com>
2022-04-10 01:08:16 +0200

    new alert text

--- src/Utils/utilities.ts
+++ src/Utils/utilities.ts
@@ -1,11 +1,13 @@
 import { Currency, Timeframe } from "../types";
-const fs = require("fs"); 
+const fs = require("fs");
 
 const suppressionTime = process.env.SUPPRESSION_TIME;
 
-export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+export const delay = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
-export const movingAverage = (data: any) => data.reduce((a, b) => a + b, 0) / data.length;
+export const movingAverage = (data: any) =>
+  data.reduce((a, b) => a + b, 0) / data.length;
 
 export const std = (data: any) => {
   const sma = movingAverage(data);
@@ -32,24 +34,39 @@ export const crossedBollingerBand = (currentPrice, std, ma) => {
 export const getChannel = (timeframe: Timeframe, client: any) => {
   switch (timeframe) {
     case Timeframe.EveryFifteenMinute:
-      return client.channels.cache.find(channel=>channel.name.includes("15m")); 
+      return client.channels.cache.find((channel) =>
+        channel.name.includes("15m")
+      );
     case Timeframe.Hourly:
-      return client.channels.cache.find(channel=>channel.name.includes("1h")); 
+      return client.channels.cache.find((channel) =>
+        channel.name.includes("1h")
+      );
     case Timeframe.EveryFourthHour:
-      return client.channels.cache.find(channel=>channel.name.includes("4h"));
+      return client.channels.cache.find((channel) =>
+        channel.name.includes("4h")
+      );
   }
-}
+};
 
-export const SendAlert = (currency: Currency, channel: any, long: boolean, price: number, bbScore: number) => {
+export const SendAlert = (
+  currency: Currency,
+  channel: any,
+  long: boolean,
+  price: number,
+  bbScore: number
+) => {
+  const title = long
+    ? `${currency.name} crossed lower bolinger band`
+    : `${currency.name} crossed upper bolinger band`;
+  const tradingView =
+    "https://www.tradingview.com/chart/?symbol=:symbol:".replace(
+      ":symbol:",
+      currency.name.concat("USDT")
+    );
+  const color = long ? 3066993 : 10038562;
 
-  const title = long ? 
-  `Possible long setting up for ${currency.name}` : 
-  `Possible short setting up for ${currency.name}`;
-  const tradingView = "https://www.tradingview.com/chart/?symbol=:symbol:".replace(":symbol:", currency.name.concat("USDT"));
-  const color = long ? 3066993 : 10038562
-  
-  if(process.env.NODE_ENV === "development") {
-    console.log(title); 
+  if (process.env.NODE_ENV === "development") {
+    console.log(title);
     return;
   }
 
@@ -57,34 +74,42 @@ export const SendAlert = (currency: Currency, channel: any, long: boolean, price
     embeds: [
       {
         title,
-          color,
-          fields: [{
-            name: "Price", 
-            value: price.toString(),
-            inline: true
-          }, 
+        color,
+        fields: [
           {
-          name: "Bolinger band score",
-          value: bbScore.toFixed(2),
-          inline: true
+            name: "Price",
+            value: price.toString(),
+            inline: true,
+          },
+          {
+            name: "Bolinger band score",
+            value: bbScore.toFixed(2),
+            inline: true,
           },
           {
             name: "Trading view",
             value: tradingView,
-            inline: false
-            }]
-      }
-    ]
+            inline: false,
+          },
+        ],
+      },
+    ],
   });
-}
+};
 
-export const shouldPerformAnalysis = (currency: Currency, timeframe: Timeframe) => {
-
+export const shouldPerformAnalysis = (
+  currency: Currency,
+  timeframe: Timeframe
+) => {
   const strTimeFrame = timeframe.toString();
 
-  if(!currency.lastTriggered.get(strTimeFrame)) {
-      return true; 
+  if (!currency.lastTriggered.get(strTimeFrame)) {
+    return true;
   }
 
-  return currency.lastTriggered.get(strTimeFrame).getTime() + parseInt(suppressionTime) < Date.now();
-}
+  return (
+    currency.lastTriggered.get(strTimeFrame).getTime() +
+      parseInt(suppressionTime) <
+    Date.now()
+  );
+};
